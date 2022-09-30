f42fc4a80a59099b1b1ba76a45e58734b0ef227f
Dinesh <dineshthorat402@gmail.com>
2022-09-30 07:15:03 +0530

    modifiyng

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
+import './Login.css';
 
 function Login() {
   return (
-    <div>
-      login
-    </div>
+    <Container>
+      <Row>
+        <Col md={5} className="login_bg" />
+        <Col md={7} className="d-flex flex-direction-column align-items-center justify-content-center" >
+
+          <Form style={{ width: "80%", maxWidth: 500 }}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+            <div className='py-4'>
+              <p className="text-center">
+                Don't have an account ? <Link to="/Signup">Signup</Link>
+              </p>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,61 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
+import "./Signup.css";
+import botImg from '../assets/robot.avif';
+
+function validateImg(){
+
+}
 
 function Signup() {
   return (
-    <div>
-      Signup
-    </div>
+    <Container>
+      <Row>
+        <Col md={7} className="d-flex flex-direction-column align-items-center justify-content-center" >
+
+          <Form style={{ width: "80%", maxWidth: 500 }}>
+            <h1 className='text-center'>Create Account</h1>
+            <div className="signup-profile-pic_container">
+              <img src={botImg} alt="#" className='signup-profile-pic' />
+              <label htmlFor="image-upload" className='image-upload-label'>
+                <i className="fas fa-plus-circle add-picture-icon"></i>
+              </label>
+              <input type="file" id='image-upload' hidden accept="image/png, image/jpeg" onChange={validateImg} />
+            </div>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Your Name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Create Account
+            </Button>
+            <div className='py-4'>
+              <p className="text-center">
+                Already have an account ? <Link to="/login">Login</Link>
+              </p>
+            </div>
+          </Form>
+        </Col>
+        <Col md={5} className="signup_bg" />
+
+      </Row>
+    </Container>
   );
 }
 
